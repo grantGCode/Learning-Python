a6fc973f13ee39473bb42dcb6f6284f5aeb70e13
Grant Bentley <gbentley0618@comcast.net>
2024-09-26 09:53:55 -0500

    Lesson02 complete

---
+++ Lesson02/welcome.py
@@ -0,0 +1,12 @@
+line01 = "********************" # Header / Footer
+line02 = "*                   *"# re-use
+line03 = "*     WELCOME!      *"
+
+
+#starts with a blank Line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
