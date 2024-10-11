882f55d3f45a2c255514bc3c2003118cfe6dd810
Grant Bentley <gbentley0618@comcast.net>
2024-10-11 15:46:02 -0500

    Lesson06 complete

--- Lesson06/Lists.py
+++ Lesson06/Lists.py
@@ -96,6 +96,8 @@ print(nums) # Returns: [5, 1, 78, 42, 4]
 print(sorted(nums, reverse=True)) # Return:  [78, 42, 5, 4, 1]
 print(nums) # Returns: [5, 1, 78, 42, 4]
 
+#Making copies of lists
+
 numscopy = nums.copy() # Creates a copy of nums
 mynums = list(nums) # Creates a copy of nums using the list() constructor.
 mycopy = nums[:] # Will splice nums and add all the data of numbs to mycopy list as a copy of nums. 
@@ -104,5 +106,58 @@ print(mynums) # Returns: [5, 1, 78, 42, 4]
 print(mycopy) # Returns: [5, 1, 78, 42, 4]
 print(nums) # Returns: [5, 1, 78, 42, 4]
 # All 4 look the same but are not the same top 3 are copies of nums.
+
+mycopy.sort() # Here I am sorting mycopy just following along.
+print(mycopy) # Returns: [1, 4, 5, 42, 78]
+
+# Displaying list types
+
+print(type(nums)) # Returns: <class 'list'>
+
+# crating list with constructor
+
+mylist =list([1, "Neil", True])
+print(mylist)
+
+# Tuples 
+#tuples use the () than [] for lists
+
+# Tuples are like lists but are different, the data inside tuples will not change and the order the data is in will not change.
+
+mytuple = tuple(('Grant', 28, True)) # Creating tuple with the tuple constructor.
+
+anothertuple = (1,4,2,8) # Creating a tuple with out a constructor.
+
+print(type(mytuple)) # Returns: <class 'tuple'>
+print(type(anothertuple)) # Returns: <class 'tuple'>
+
+# If I do need to change the content of a tuple or "Pack the tuple." The best way to go about that is to copy the tuple to a new list the create a new tuple by copying that list you just created.
+
+newlist = list(mytuple) # Copy tuple to a list.
+newlist.append('Neil') # Append the new value to the newlist.
+newtuple = tuple(newlist) # Then I can make a new tuple out of the content of newlist.
+print(newtuple) # Returns: ('Grant', 28, True, 'Neil')
+
+# Unpacking a tuple
+
+(one, two, *hey) = anothertuple # See line 129 for anothertuple
+print(one) # Returns: 1
+print(two) # Returns: 4
+print(hey) # Returns: [2, 8]
+
+# or
+
+# (one, *two, hey) = anothertuple # See line 129 for anothertuple
+# print(one) # Returns: 1
+# print(two) # Returns: [4, 2]
+# print(hey) # Returns: 8
+
+# The * next to the tuple value will retune the remaining values in the tuple in to a list as seen above.
+
+counttuple = (1,4,2,8,2,2)
+print(counttuple.count(2)) # Returns: 3
+# count will show how may occurrences of a value are in the tuple or list.
+
+
 
 
