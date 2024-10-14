f827e89e2af51992b662587c42643be70b23e3ad
Grant Bentley <gbentley0618@comcast.net>
2024-10-14 12:50:20 -0500

    save notes on checking pram types and passing in arguments

--- Lesson09/functions.py
+++ Lesson09/functions.py
@@ -9,7 +9,12 @@ hello_world()
 
 # Basic function syntax with params an arguments in python.
 def sum(num1, num2):
+    if(type(num1) is not int or type(num2) is not int):
+        return 
+    # None (is a default value in python if you add an early return just like on line 13.)
     return num1 + num2
 
 total = sum(2,3)
 print(total)
+print(sum("a",3))
+
