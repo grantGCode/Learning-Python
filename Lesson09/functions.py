ba9ee8f5db70e45b31ece286d2fa6388e488aa0f
Grant Bentley <gbentley0618@comcast.net>
2024-10-14 12:40:12 -0500

    save notes on basice function syntax in python

---
+++ Lesson09/functions.py
@@ -0,0 +1,15 @@
+# When naming function names need to be all lowercase with for multiple words  separated by underscores.
+# No capital letters or - hyphen   
+
+# Basic function syntax in python.
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+# Basic function syntax with params an arguments in python.
+def sum(num1, num2):
+    return num1 + num2
+
+total = sum(2,3)
+print(total)
